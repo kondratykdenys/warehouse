851f3bb995436753417b2140809ae906e8373afb
kondratykDenys <kondratyk.denys@gmail.com>
2020-12-12 12:51:27 +0200

    first deploy to heroku

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ const express = require("express")
 const config = require("config")
 const bodyParser = require("body-parser")
 const db = require("./models")
+const path = require('path')
 
 const app = express()
 
 app.use(bodyParser.json())
-app.use(express.static("public"))
 
 app.use("/api/auth", require("./routes/auth.routes"))
 
-const PORT = config.get("port") || 3306
+const PORT = process.env.PORT || 3306
+
+if ((process.env.NODE_ENV = "production")) {
+  app.use(express.static("client/build"))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  });
+}
 
 async function start() {
   try {
